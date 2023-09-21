8e956250afd63761d67ba016ed35b9be339fa27c
MichaelChemo2507 <mnh8551@gmail.com>
2023-09-21 21:09:51 +0300

    finished employees CRUD

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 let db_M = require('./database');
 global.db_pool = db_M.pool;
 
+const emp_rtr = require('./routers/emp_rout');
+app.use('/Employees', emp_rtr);
+
+
 app.listen(port, () => {
     console.log(`Now listening on port http://localhost:${port}`);
 });
