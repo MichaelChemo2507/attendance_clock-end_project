24fc751165c3de77f2f4705e01d4bdd875ae8d84
MichaelChemo2507 <mnh8551@gmail.com>
2023-09-21 21:06:49 +0300

    added db file and linked it.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({extended: false}));
 
+let db_M = require('./database');
+global.db_pool = db_M.pool;
 
 app.listen(port, () => {
     console.log(`Now listening on port http://localhost:${port}`);
