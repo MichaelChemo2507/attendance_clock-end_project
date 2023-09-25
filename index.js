e9045e490964b79f6ed321e9084f9479733d441f
MichaelChemo2507 <mnh8551@gmail.com>
2023-09-25 23:35:06 +0300

    started to work on the employees front

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ let db_M = require('./database');
 global.db_pool = db_M.pool;
 
 const emp_rtr = require('./routers/emp_rout');
-app.use('/Employees', emp_rtr);
+app.use('/Employees',emp_rtr);
 const clock_rtr = require('./routers/clock_rout');
-app.use('/Clock', clock_rtr);
+app.use('/Clock',clock_rtr);
+const front_rtr = require('./routers/FE_R');
+app.use('/Front',front_rtr);
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "js")));
+app.set("view engine","ejs");
 
 app.listen(port, () => {
     console.log(`Now listening on port http://localhost:${port}`);
