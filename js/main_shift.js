3cb5586394a00c4813239314f15bd6705e7dbaa1
MichaelChemo2507 <mnh8551@gmail.com>
2023-09-26 19:13:30 +0300

    started to work on the shift's buttons.

--- js/main_shift.js
+++ js/main_shift.js
@@ -4,6 +4,16 @@ exitBtn = document.getElementsByTagName("button")[1];
 
 getEmpId();
 
+entryBtn.addEventListener("click", async ()=>{
+    let response = await fetch(`http://localhost:3507/Clock/Entry/${EmpNames.value}`,{
+        method:'POST',
+        headers:{
+            'content-Type':'application/json'
+        },
+        body:JSON.stringify({
+        })
+    })
+})
 async function getEmpId(){
     let response = await fetch("http://localhost:3507/Employees/List");
     let data = await response.json();
