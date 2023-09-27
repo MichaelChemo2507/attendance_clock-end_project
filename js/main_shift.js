60bc613346d0d05570baae7fce449eab94119155
MichaelChemo2507 <mnh8551@gmail.com>
2023-09-27 09:25:42 +0300

    working on the shift's buttons.

--- js/main_shift.js
+++ js/main_shift.js
@@ -1,7 +1,10 @@
 let EmpNames = document.getElementById("empNames"),
 entryBtn = document.getElementsByTagName("button")[0],
-exitBtn = document.getElementsByTagName("button")[1];
+exitBtn = document.getElementsByTagName("button")[1],
+date = new Date();
 
+
+let currentDate =   `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 getEmpId();
 
 entryBtn.addEventListener("click", async ()=>{
@@ -11,14 +14,18 @@ entryBtn.addEventListener("click", async ()=>{
             'content-Type':'application/json'
         },
         body:JSON.stringify({
+            Entry_time:currentDate
         })
     })
 })
+
+
+
 async function getEmpId(){
     let response = await fetch("http://localhost:3507/Employees/List");
     let data = await response.json();
     let NameList = "";
-    for (const item of data) {
+    for (const item of data.rows) {
         NameList+=`<option value="${item.Employee_id}">${item.FirstName} ${item.LastName}</option>`;
     }
     EmpNames.innerHTML = NameList ;
